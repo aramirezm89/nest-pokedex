2752061925878504684541d9644c9f180a95c4d7
aramirezm <aramirez.unimarc@gmail.com>
2025-09-18 22:48:37 -0300

    feat: add ConfigModule and move MongoDB connection string to environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://aramirezm:Ramirez1989@localhost:27017/?authSource=admin',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI || ''),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
