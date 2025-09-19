144dd7c704f69eb3f2534fe3dabff26d69e934e0
aramirezm <aramirez.unimarc@gmail.com>
2025-09-18 22:39:18 -0300

    feat: add MongoDB integration with Mongoose and connection config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
+    MongooseModule.forRoot(
+      'mongodb://aramirezm:Ramirez1989@localhost:27017/?authSource=admin',
+    ),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
